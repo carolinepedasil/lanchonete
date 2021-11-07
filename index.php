f26291b2de037c0ebc39b586749c8387d17cf2f8
Caroline P. da Silva <carolinepedasil@gmail.com>
2021-11-07 19:10:23 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 <body>
+    <div class="container">
     <h2>Efetue login</h2>
     <form id="form_login" action="login.php" method="POST">
         <?php if(isset($resultado) && $resultado["cod"] == 0): ?>
@@ -17,9 +18,10 @@
         <input type="email" id="email" name="email" placeholder="Digite seu email" />
         <br/><br/>
         <input type="password" id="senha" name="senha" placeholder="Digite sua senha" />
-        <br/>
-        <input type="submit" id="submeter" value="Entrar" />
+        <br/><br/>
+        <input type="submit" id="submeter" value="Entrar" class="btn btn-primary" />
     </form>
+    </div>
 </body>
 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
