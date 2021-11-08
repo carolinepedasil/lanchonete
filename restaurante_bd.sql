4821ba45d6ff8661c7d3380d80c6571ab839af11
Caroline P. da Silva <carolinepedasil@gmail.com>
2021-11-08 18:02:28 -0300

    Add files via upload

--- restaurante_bd.sql
+++ restaurante_bd.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Tempo de geração: 08-Nov-2021 às 21:42
+-- Tempo de geração: 08-Nov-2021 às 22:02
 -- Versão do servidor: 10.4.20-MariaDB
 -- versão do PHP: 8.0.9
 
@@ -30,12 +30,20 @@ SET time_zone = "+00:00";
 CREATE TABLE `item_pedido` (
   `codigo` int(11) NOT NULL,
   `nome_produto` varchar(100) DEFAULT NULL,
-  `quantidade` int(11) NOT NULL,
+  `quantidade` int(11) DEFAULT NULL,
   `preco_und` decimal(6,2) NOT NULL,
-  `observacao` varchar(200) DEFAULT NULL,
+  `observacao` varchar(200) NOT NULL,
   `cod_usuario` int(11) DEFAULT NULL,
   `data_hora` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `item_pedido`
+--
+
+INSERT INTO `item_pedido` (`codigo`, `nome_produto`, `quantidade`, `preco_und`, `observacao`, `cod_usuario`, `data_hora`) VALUES
+(1, NULL, NULL, '29.90', '', NULL, '2021-11-08 20:54:04'),
+(2, NULL, NULL, '29.90', '', NULL, '2021-11-08 21:00:54');
 
 -- --------------------------------------------------------
 
@@ -84,7 +92,7 @@ ALTER TABLE `usuario`
 -- AUTO_INCREMENT de tabela `item_pedido`
 --
 ALTER TABLE `item_pedido`
-  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `codigo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de tabela `usuario`
