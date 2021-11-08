aeb3ceef9e49b7acaed486122747a10085b8feb6
Caroline P. da Silva <carolinepedasil@gmail.com>
2021-11-08 18:41:01 -0300

    Add files via upload

--- cadastrar_pedido.php
+++ cadastrar_pedido.php
@@ -1,40 +1,41 @@
 <?php
+
     if(count($_POST) > 0) {
-        // 1. Pegar os valores do formulário
-        $nome  = $_POST["nome_produto"];
-        $qtd   = $_POST["qtd_produto"];
-        $obs   = $_POST["obs_produto"];
-        $preco = $_POST["preco_produto"];
-        // TODO pegar o código do usuário logado (chave estrangeira)
-        
-        // 2. Conexão com o banco de dados
-        $servename = "localhost";
-        $username = "root";
-        $password = "";
+        // 1. Pegar os dados do formulário
+        $email = $_POST['nome_produto'];
+        $qtd = $_POST['qtd_produto'];
+        $obs = $_POST['obs_produto'];
+        $preco = $_POST['preco_produto'];
+        // Pegar o código do usuário logado (chave)
 
+        // Nas linhas de baixo vai tentar fazer conexão
         try {
-            $conn = new PDO("mysql:host=$servename;dbname=restaurante_bd", $username, $password);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            // echo "Conexão realizada com sucesso.";
+            include("conexao_bd.php");
+            // echo "Conexão realizada com sucesso!"; 
 
-            // 3. Verificar se email e senha estão no banco de dados
+            // 3. Verificar se o email e a senha estão no banco de dados
             $sql = "INSERT INTO item_pedido 
-                    (cod_usuario, nome_produto, observacao, preco_und, quantidade) 
+                    (cod_usuario, nome_produto, observacao, preco, quantidade) 
                     VALUES 
                     (?,?,?,?,?)";
-            $stmt= $conn->prepare($sql);
-            // TODO pegar o código do usuário logado:
-            $stmt->execute([null, $nome, $obs, $preco, $qtd ]);
-        
-            $resultado["msg"] = "Item inserido!";
+            $stmt = $conn->prepare($sql);
+            $stmt->execute([null, $nome, $obs, $preco, $qtd]);
+
+            //se der certo:
+            $resultado["msg"] = "Item inserido com sucesso!";
             $resultado["cod"] = 1;
+            $resultado["style"] = "alert-success";
+
         }
-        catch(PDOException $e){
-            echo "Inserção no banco de dados falhou: " . $e->getMessage();
-            $resultado["msg"] = "Item não inserido";
+        //se falhar:
+        catch(PDOException $e) {
+            $resultado["msg"] = "Inserção no Banco de Dados falhou: " . $e->getMessage();
             $resultado["cod"] = 0;
+            $resultado["style"] = "alert-danger";
         }
         $conn = null;
     }
+
+    include("pedido.php");
 
 ?>
