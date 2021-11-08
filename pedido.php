aa39e8c20d456734c6fab9b0eb8902c94dfd4c1f
Caroline P. da Silva <carolinepedasil@gmail.com>
2021-11-08 18:41:29 -0300

    Add files via upload

--- pedido.php
+++ pedido.php
@@ -1,38 +1,46 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 </head>
+
 <body>
     <div class="container">
-        <form action="cadastrar_pedido.php" method="POST">
-        <h2>Escolha de itens do pedidos</h2>
+        <h2>Escolha de itens do pedido</h2>
         <br/>
-        <div class="form-group">
-            <label for="nome_produto">Nome do produto:</label>
-            <input type="text" class="form-control" id="nome_produto" placeholder="Digite o produto">
-        </div>
-        <div class="form-group">
-            <label for="qtd_produto">Quantidade:</label>
-            <input type="number" class="form-control" id="qtd_produto" maxlengh="10">
-        </div>
-        <div class="form-group">
-            <label for="obs_produto">Observação:</label>
-            <input type="textarea" class="form-control" id="obs_produto" name="obs_produto">
-        </div>
-        <div class="form-group">
-            <label for="preco_produto">Preço unitário:</label>
-            <input type="text" class="form-control" id="preco_produto" name="preco_produto" >
-        </div>
-        <button type="submit" class="btn btn-primary">Adicionar item</button>
-
+        <form action="cadastar_pedido.php" method="POST">
+            <div class="form-group">
+                <label for="nome_produto">Nome do produto: </label>
+                <input type="text" class="form-control" id="nome_produto" name="nome_produto" placeholder="Digite produto">
+            </div>
+            <div class="form-group">
+                <label for="qtd_produto">Quantidade: </label>
+                <input type="number" class="form-control" id="qnt_produto" name="qtd_produto" maxlength="10">
+            </div>
+            <div class="form-group">
+                <label for="qtd_produto">Observação: </label>
+                <input type="text" class="form-control" id="obs_produto" name="obs_produto">
+            </div>
+            <div class="form-group">
+                <label for="preco_produto">Preço: </label>
+                <input type="text" class="form-control" id="preco_produto" name="preco_produto"> <!--- type="number" não permitiu adicionar virgula, então coloquei type="text" mesmo -->
+            </div>
+            <button type="submit" class="btn btn-primary">Adicionar item</button>
+            <?php if( isset($resultado) ): ?>
+                    <div class="alert <?=$resultado["style"]?> ">
+                        <?php echo $resultado["msg"]; ?>
+                    </div>
+            <?php endif; ?>
         </form>
     </div>
 </body>
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
 </html>
